092a220d7e538d380625c6293b080108aeccb3fa
Marcus Svensson <macke77@gmail.com>
2014-10-14 20:43:40 +0200

    Read settings from chrome.storage.local

--- re-direct.js
+++ re-direct.js
@@ -1,39 +1,42 @@
 (function(d) {
   'use strict';
 
-  var hideReferrer = true;
+  chrome['storage']['local'].get('hideReferrer', function(settings) {
 
-  function cleanLink(a) {
-    var data = a.dataset;
-    if (data && data.href && '/url' === a.pathname) {
-      a.href = data.href;
-      if (hideReferrer)
-        a.rel = 'noreferrer';
+    var hideReferrer = (false !== settings['hideReferrer']);
+
+    function cleanLink(a) {
+      var data = a.dataset;
+      if (data && data.href && '/url' === a.pathname) {
+        a.href = data.href;
+        if (hideReferrer)
+          a.rel = 'noreferrer';
+      }
     }
-  }
 
-  (function() {
-    var i = 0, elems = d.getElementsByTagName('a');
-    while (i<elems.length)
-      cleanLink(elems[i++]);
-  }());
+    (function() {
+      var i = 0, elems = d.getElementsByTagName('a');
+      while (i<elems.length)
+        cleanLink(elems[i++]);
+    }());
 
-  var observer = new MutationObserver(function(mutations) {
-    observer.disconnect();
-    mutations.forEach(function(mrec) {
-      cleanLink(mrec.target);
+    var observer = new MutationObserver(function(mutations) {
+      observer.disconnect();
+      mutations.forEach(function(mrec) {
+        cleanLink(mrec.target);
+      });
+      startObserving();
     });
+
+    function startObserving() {
+      observer.observe(d, {
+        attributes: true,
+        subtree: true,
+        attributeFilter: ['href']
+      });
+    }
+
     startObserving();
   });
-
-  function startObserving() {
-    observer.observe(d, {
-      attributes: true,
-      subtree: true,
-      attributeFilter: ['href']
-    });
-  }
-
-  startObserving();
 
 }(document));
